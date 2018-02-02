6b892ec226064aa973d0d07b5256baba425497a9
Vladimir Iglovikov <iglovikov@gmail.com>
2018-02-01 21:12:22 -0800

    bugfix in randomcrop

--- transforms.py
+++ transforms.py
@@ -289,11 +289,8 @@ class RandomCrop:
     def __call__(self, img, mask=None):
         h, w, c = img.shape
 
-        start_height = np.random.randint(0, h - self.height - 1)
-        start_width = np.random.randint(0, w - self.width - 1)
-
-        dy = (h - self.height) // 2
-        dx = (w - self.width) // 2
+        start_height = np.random.randint(0, h - self.height)
+        start_width = np.random.randint(0, w - self.width)
 
         y1 = start_height
         y2 = y1 + self.height
