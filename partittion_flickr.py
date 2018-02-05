103b21f29468b6361f2f127c37131bdf4d6cc8ee
Vladimir Iglovikov <iglovikov@gmail.com>
2018-02-04 17:02:37 -0800

    bugfix

--- partittion_flickr.py
+++ partittion_flickr.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 from joblib import Parallel, delayed
 
 
-def helper(file_name):
+def helper1(file_name):
     img = cv2.imread(str(file_name))
 
     h, w, c = img.shape
@@ -21,6 +21,22 @@ def helper(file_name):
                 [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
 
+def helper2(file_name):
+    img = cv2.imread(str(file_name))
+
+    h, w, c = img.shape
+    h_step = int(h / 3)
+    w_step = int(w / 3)
+
+    for h in range(0, h - h_step, h_step):
+        for w in range(0, w - w_step, w_step):
+            im = img[h:h + h_step, w:w + w_step, :]
+
+            cv2.imwrite(
+                str(new_train_path / class_name.name / (str(h) + '_' + str(w) + '_' + file_name.stem + '.jpg')), im,
+                [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+
+
 data_path = Path('data')
 
 flickr_path = data_path / 'new_flickr'
@@ -28,17 +44,14 @@ flickr_path = data_path / 'new_flickr'
 new_flickr_path = data_path / 'new_flickr2'
 new_flickr_path.mkdir(exist_ok=True)
 
-
 for class_name in tqdm(list(flickr_path.glob('*'))):
     (new_flickr_path / class_name.name).mkdir(exist_ok=True)
-    Parallel(n_jobs=12)(delayed(helper)(file_name) for file_name in list(class_name.glob('*')))
-
+    Parallel(n_jobs=12)(delayed(helper1)(file_name) for file_name in list(class_name.glob('*')))
 
 train_path = data_path / 'train'
 new_train_path = data_path / 'train2'
 new_train_path.mkdir(exist_ok=True)
 
-
 for class_name in tqdm(list(train_path.glob('*'))):
     (new_train_path / class_name.name).mkdir(exist_ok=True)
-    Parallel(n_jobs=12)(delayed(helper)(file_name) for file_name in list(class_name.glob('*')))
+    Parallel(n_jobs=12)(delayed(helper2)(file_name) for file_name in list(class_name.glob('*')))
