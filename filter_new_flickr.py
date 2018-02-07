69e095fbed9437e4b4c465d526c18e0078c689cb
Vladimir Iglovikov <iglovikov@gmail.com>
2018-02-07 05:09:08 -0800

    New flickr filtering works in parallel

--- filter_new_flickr.py
+++ filter_new_flickr.py
@@ -1,17 +1,20 @@
 from pathlib import Path
 import jpeg4py
 import os
-
-from tqdm import tqdm
+from joblib import Parallel, delayed
 
 data_path = Path('data')
 train_path = data_path / 'new_flickr'
 
 train_file_names = list(train_path.glob('**/*.*'))
 
-for file_name in tqdm(train_file_names):
+
+def helper(file_name):
     try:
         x = jpeg4py.JPEG(str(file_name)).decode()
     except:
-        print(file_name)
         os.remove(str(file_name))
+
+
+Parallel(n_jobs=20)(delayed(helper)(x) for x in train_file_names)
+
